5596eae64fdf38720a2becacc9eebaf71c1ba6e2
Luca Lambia <luca@lambia.it>
2026-02-25 10:02:07 +0100

    semplificato

--- main.js
+++ main.js
@@ -3,31 +3,12 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			name: null,
-			surname: null,
-			// oggetto: {}
-			titleColor: "blue",
-			isFormVisible: true,
-			todoList: [
-				"finire lezione",
-				"salutarsi",
-				"mangiare"
-			],
-			qualcosa: ""
+			payload: "testo salvato in data"
 		}
 	},
 	methods: {
 		appInit() {
-			console.log("Applicazione montata correttamente", this.name);
-			// this.name = "Mario"
-		},
-		onButtonClicked() {
-			console.log("Bottone cliccato");
-
-			this.titleColor = (this.titleColor.target === "red") ? "blue" : "red";
-		},
-		onShowHideClicked() {
-			this.isFormVisible = !this.isFormVisible;
+			console.log("Applicazione montata correttamente");
 		}
 	},
 	mounted() {
