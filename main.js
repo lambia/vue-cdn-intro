3ef04a2f00e57ae186146f11218d229a749460eb
Luca Lambia <luca@lambia.it>
2026-02-25 11:26:20 +0100

    feat: add and clear tasks

--- main.js
+++ main.js
@@ -3,12 +3,54 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			payload: "testo salvato in data"
+			newTaskTitle: "",
+			todoList: [
+				"Completare lo sviluppo",
+				"Testare l'applicazione",
+				"Rilasciare in QA",
+			],
+			validationClass: "",
+			// todoList: [
+			// 	{
+			// 		title: "Completare lo sviluppo",
+			// 		description: "Completare lo sviluppo: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptate.",
+			// 		completed: true
+			// 	},
+			// 	{
+			// 		title: "Testare l'applicazione",
+			// 		description: "Testare l'applicazione: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptate.",
+			// 		completed: false
+			// 	},
+			// 	{
+			// 		title: "Rilasciare in QA",
+			// 		description: "Rilasciare in QA: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptate.",
+			// 		completed: false
+			// 	}
+			// ]
 		}
 	},
 	methods: {
 		appInit() {
-			console.log("Applicazione montata correttamente");
+			console.log("Applicazione montata correttamente", this.todoList);
+		},
+		addTask() {
+			//svuota
+			this.todoList.push(this.newTaskTitle);
+			this.newTaskTitle = "";
+		},
+		clearTasks() {
+			this.todoList = [];
+		},
+		getValidation() {
+			let className = "";
+
+			if (this.newTaskTitle.trim().length < 3) {
+				className = "newtask-input-error"
+			} else {
+				className = "newtask-input-success"
+			}
+
+			return className;
 		}
 	},
 	mounted() {
